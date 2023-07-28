36ec0d4f754ba94dd6e704e72026f8fea63ee9ef
Alberto Alv <albertoalv@MacBook-Pro.local>
2023-07-28 13:43:11 -0600

    arrgelando bugs

--- app.py
+++ app.py
@@ -15,14 +15,22 @@ def home():
         # Por ejemplo, una simple respuesta automática
         if user_input.lower() == 'hola':
             respuesta = "¡Hola! ¿Cómo estás?"
+            respuestas_chat.append(("Tu", user_input))
+            respuestas_chat.append(("Agavi", respuesta))
+        elif user_input.lower() == 'limpiar':
+            respuestas_chat = []
         else:
             respuesta = "Gracias por tu mensaje: " + user_input
+            respuestas_chat.append(("Tu", user_input))
+            respuestas_chat.append(("Agavi", respuesta))
         
         # Agregar la respuesta del usuario y la respuesta del chat a la lista
-        respuestas_chat.append(("Tu", user_input))
-        respuestas_chat.append(("Agavi", respuesta))
+        
+    if request.method == 'GET':
+        respuestas_chat.clear()
 
     return render_template('index.html', respuestas_chat=respuestas_chat)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
